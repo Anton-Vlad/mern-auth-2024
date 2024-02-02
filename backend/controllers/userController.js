23fd73e45207fa4ecbd28412883838a4142f847b
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-02-02 22:17:06 +0200

    Added basic api auth routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,13 +4,42 @@ import asyncHandler from 'express-async-handler';
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(401);
-    throw new Error('Something is fishy')
-
     res.status(200).json({ message: 'Auth User' })
+});
+
+// @des     Register a new user
+// route    POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register User' })
+});
+
+// @des     Logout user
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout User' })
+});
+
+// @des     Get user profile
+// route    GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile' })
+});
+
+// @des     Update user profile
+// route    PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile Updated' })
 });
 
 
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 };
