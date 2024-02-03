14810988433b252123062b474191c826ff203679
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-02-03 14:52:25 +0200

    React router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header></Header>
-      <HomeScreen />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   )
 }
